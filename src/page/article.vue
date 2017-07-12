138703c7d4248b73852bef44c085c6b70af459d0
Recklesslmz <936838918@qq.com>
2017-07-12 12:28:50 +0800

    fixed

--- src/page/article.vue
+++ src/page/article.vue
@@ -6,6 +6,16 @@
 </template>
 
 <script type="text/ecmascript-6">
+  function* helloWorld () {
+    yield 'hello'
+    yield 'world'
+    return 'ending'
+  }
+  var hw = helloWorld()
+
+  console.log(hw.next())
+  console.log(hw.next())
+  console.log(hw.next())
   export default{
     mounted(){
     },
