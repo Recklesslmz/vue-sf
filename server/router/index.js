2370687dfddf720bd52e2dc36601480f0a211e8f
Recklesslmz <936838918@qq.com>
2017-07-18 20:27:08 +0800

    add server

--- server/router/index.js
+++ server/router/index.js
@@ -2,3 +2,7 @@
  * Created by limuzi on 2017/7/13.
  */
 
+const server = require('./server')
+const router = require('./router')
+
+server.start(router.route)
---
+++ server/router/router.js
@@ -0,0 +1,8 @@
+/**
+ * Created by limuzi on 2017/7/18.
+ */
+function route(pathname) {
+  console.log("About to route a request for " + pathname);
+}
+
+exports.route = route
